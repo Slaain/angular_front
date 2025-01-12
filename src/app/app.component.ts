96e25b55d3828c4a246301903301fa4c9ae7a93d
slaain <idris1390@gmail.com>
2025-01-13 00:32:21 +0100

    landing page ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [  HeaderComponent, RouterOutlet ], //importer les dépendences
+  imports: [  HeaderComponent, RouterOutlet ], //imporsster les dépendences
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
