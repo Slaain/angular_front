f3f6100208d4cf4f1966d650a3e20755c83ffcb5
slaain <idris1390@gmail.com>
2025-01-18 21:22:34 +0100

    mise en places des différents router

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
 
+  constructor(private router: Router) {
+
+  }
+  onContinue( ) {
+this.router.navigate(['facesnaps']);
+  }
 }
